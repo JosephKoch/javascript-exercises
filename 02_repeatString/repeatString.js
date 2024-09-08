e14df02d3ffd00de10eb71bdd651218f82e43b99
JosephKoch <131320917+JosephKoch@users.noreply.github.com>
2024-09-08 15:47:55 -0700

    Add Error condition for negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,9 +3,12 @@ let text = "";
 let arrString = [];
 
 const repeatString = function(String, Count) {
+    if (Count < 0) {
+        return "ERROR";
+    } else {
     for (let i = 1; i <= Count; i++) { 
         text = text.concat(String);
-}
+}}
     return text;
 };
 
