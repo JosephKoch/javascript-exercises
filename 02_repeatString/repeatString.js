a774435973bd9762da4c90b5dcfc56ae1528cd11
JosephKoch <131320917+JosephKoch@users.noreply.github.com>
2024-09-08 16:21:26 -0700

    Create if statement to reset text repeatString
    If the repeatString function is used more than once

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,16 +1,23 @@
 
 let text = "";
-let arrString = [];
 
 const repeatString = function(String, Count) {
     if (Count < 0) {
         return "ERROR";
     } else {
     for (let i = 1; i <= Count; i++) { 
-        text = text.concat(String);
+        if (i === 1) {
+            text = "";
+            text = text.concat(String);
+        } else {
+            text = text.concat(String);
+        };
 }}
     return text;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
