9048c77ae0c1bcce9263cb76bbc7d09539201404
JosephKoch <131320917+JosephKoch@users.noreply.github.com>
2024-09-08 15:37:14 -0700

    Add for loop to repeat strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
 
+let text = "";
+let arrString = [];
+
+const repeatString = function(String, Count) {
+    for (let i = 1; i <= Count; i++) { 
+        text = text.concat(String);
+}
+    return text;
 };
 
 // Do not edit below this line
