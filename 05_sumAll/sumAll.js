e5e0cbb5fa7b3946e8699d7ada6db352e4adb88a
JosephKoch <131320917+JosephKoch@users.noreply.github.com>
2024-09-08 20:30:35 -0700

    pseudo code for sum of all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,5 +2,11 @@ const sumAll = function() {
 
 };
 
+//create function low and high argument
+//increment from low to high number.
+//add each increment to total sum
+//return sum
+
+
 // Do not edit below this line
 module.exports = sumAll;
