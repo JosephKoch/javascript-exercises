93dc682ee4c2003c515f6927845b920e9ca2ab96
JosephKoch <131320917+JosephKoch@users.noreply.github.com>
2024-09-08 19:23:05 -0700

    Create new array with to filter arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, x) {
+    let arr = myArray;
+    arr = arr.filter(e => e !== x);
+    return arr;
 };
 
 // Do not edit below this line
